df1960c679baaf991bf7e096c462ade5710f7d3f
vivis2kuni <viviana.fraccarolli@studenti.unitn.it>
2025-12-28 01:05:39 +0100

    error fixing

--- target/debug/deps/planet_luna4-5723edd6b5d95966.d
+++ target/debug/deps/planet_luna4-5723edd6b5d95966.d
@@ -1,6 +1,12 @@
-D:\S2k in your area\pelatone\Planet_Luna4\target\debug\deps\planet_luna4-5723edd6b5d95966.d: src\main.rs src\ai.rs
+D:\S2k in your area\pelatone\Planet_Luna4\target\debug\deps\planet_luna4-5723edd6b5d95966.d: src\main.rs src\planet\mod.rs src\planet\cycle.rs src\planet\energy.rs src\planet\errors.rs src\planet\resources.rs src\planet\state.rs src\logging.rs
 
-D:\S2k in your area\pelatone\Planet_Luna4\target\debug\deps\libplanet_luna4-5723edd6b5d95966.rmeta: src\main.rs src\ai.rs
+D:\S2k in your area\pelatone\Planet_Luna4\target\debug\deps\libplanet_luna4-5723edd6b5d95966.rmeta: src\main.rs src\planet\mod.rs src\planet\cycle.rs src\planet\energy.rs src\planet\errors.rs src\planet\resources.rs src\planet\state.rs src\logging.rs
 
 src\main.rs:
-src\ai.rs:
+src\planet\mod.rs:
+src\planet\cycle.rs:
+src\planet\energy.rs:
+src\planet\errors.rs:
+src\planet\resources.rs:
+src\planet\state.rs:
+src\logging.rs:
